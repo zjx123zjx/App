c51cd8acea39e3b7603b1181e5232b5dd0c76c2a
zjx123zjx <15845941956@163.com>
2017-11-11 00:42:12 +0800

    17-11-11 0:41
    笔记功能实现

--- src/cn/itcast/xzgl/action/Action.java
+++ src/cn/itcast/xzgl/action/Action.java
@@ -1,11 +1,19 @@
 package cn.itcast.xzgl.action;
 
+import java.io.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import cn.itcast.core.action.BaseAction;
 
 public class Action extends BaseAction {
+	
 
 	public String frame(){
-		System.out.println("yes");
+		System.out.println("yes++");
 		return "frame";
 	}
 	
@@ -18,6 +26,29 @@ public class Action extends BaseAction {
 	}
 	
 	public String bj(){
+		return "bj";
+	}
+	
+	public String note(){
+		HttpServletRequest request = ServletActionContext.getRequest();
+		String str = request.getParameter("tt");
+		System.out.println(str);   
+		
+		BufferedWriter out = null;     
+        try {     
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:\\java\\myeclipse\\workspace\\itcastTax\\WebRoot\\WEB-INF\\jsp\\xzgl\\bj.txt", true)));     
+            out.write(str);     
+        } catch (Exception e) {     
+            e.printStackTrace();     
+        } finally {     
+            try {     
+                if(out != null){  
+                    out.close();     
+                }  
+            } catch (IOException e) {     
+                e.printStackTrace();     
+            }     
+        }
 		return "bj";
 	}
 	
